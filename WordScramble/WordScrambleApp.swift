d1e93910498440dcfd94e7f0f89340ee6fbf1a0e
Edwin Cardenas <eacardenase@gmail.com>
2026-04-17 17:12:28 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Edwin Cardenas on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
