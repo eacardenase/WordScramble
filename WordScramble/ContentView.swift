dafe8e5099916b60220589ad095367342a1e734a
Edwin Cardenas <eacardenase@gmail.com>
2026-04-17 21:21:20 -0500

    feat: foreach within lists for dynamic rows

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,25 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+
     var body: some View {
         List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
+            Text("Static row")
+
+            ForEach(people, id: \.self) {
+                Text($0)
             }
 
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row #\($0 + 1)")
-                }
-            }
-
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+            Text("Static row")
         }
-        .listStyle(.insetGrouped)
     }
 }
 
